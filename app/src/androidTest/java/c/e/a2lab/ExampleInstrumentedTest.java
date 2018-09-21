170d76290f05feddf9f42a872605d23ba48ea7d3
Stasana <stasanaa@gmail.com>
2018-09-21 10:03:23 +0300

    lab_2

--- app/src/androidTest/java/c/e/a2lab/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/c/e/a2lab/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package develop.mobilelab;
+package c.e.a2lab;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("develop.mobilelab", appContext.getPackageName());
+        assertEquals("c.e.a2lab", appContext.getPackageName());
     }
 }
